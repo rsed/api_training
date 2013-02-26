f604ee9d7501b1f7f67d9c7287193f7669f19dab
johnfitzpatrick <john.fitzpatrck@rightscale.com>
2013-02-26 16:31:44 +0000

    minor edit

--- authenticate/authenticate.sh
+++ authenticate/authenticate.sh
@@ -18,4 +18,5 @@ done
 EMAIL="john.fitzpatrick@rightscale.com"  # The email address for your RightScale User in the Dashboard.
 ACCOUNT="62988"                # Account ID, obtained from navigation in the Dashboard (Student 02)
 
+echo ""
 curl -i -H X_API_VERSION:1.5 -c mycookie -X POST -d email=$EMAIL -d password=$password -d account_href=/api/accounts/$ACCOUNT https://us-3.rightscale.com/api/session
