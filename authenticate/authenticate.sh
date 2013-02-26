7c06b51a2a01780035cfb394ef05a0096d778cca
johnfitzpatrick <john.fitzpatrck@rightscale.com>
2013-02-26 16:21:21 +0000

    minor edit

--- authenticate/authenticate.sh
+++ authenticate/authenticate.sh
@@ -2,8 +2,6 @@
 
 unset password
 
-#read -p "Please enter the password associated with email '$EMAIL': " password 
-
 prompt="Please enter the password associated with email '$EMAIL': "
 
 while IFS= read -p "$prompt" -r -s -n 1 char
@@ -16,9 +14,8 @@ do
     password+="$char"
 done
 
-EMAIL="john.fitzpatrick@rightscale.com"  # The email address for your RightScale User in the Dashboard
-#PSWD="$password"      # Your User's password
+# The following to get populated from a RightScript
+EMAIL="john.fitzpatrick@rightscale.com"  # The email address for your RightScale User in the Dashboard.
 ACCOUNT="62988"                # Account ID, obtained from navigation in the Dashboard (Student 02)
 
 curl -i -H X_API_VERSION:1.5 -c mycookie -X POST -d email=$EMAIL -d password=$password -d account_href=/api/accounts/$ACCOUNT https://us-3.rightscale.com/api/session
-#curl -i -H X_API_VERSION:1.5 -c mycookie -X POST -d email=$EMAIL -d password=$PSWD -d account_href=/api/accounts/$ACCOUNT https://us-3.rightscale.com/api/session
