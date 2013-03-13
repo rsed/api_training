044b03e5aadaa55ad0335414c4e604d696e06966
johnfitzpatrick <john.fitzpatrick@rightscale.com>
2013-03-13 17:37:43 +0000

    interim update

---
+++ 3tier/RightScript-APP-Import.sh
@@ -0,0 +1,10 @@
+#!/bin/bash -e 
+
+# This script is for use in the API Training Course
+# Written by John Fitzpatrick, March 2013
+
+RIGHTSCRIPTID="48616"
+
+echo "***Importing RightScript '3Tier-End-to-End Demo [RSED]'***"
+curl -i -H X_API_VERSION:1.5 -b ~/mycookie -X POST \
+https://us-3.rightscale.com/api/publications/$RIGHTSCRIPTID/import
---
+++ 3tier/ServerAPPs-RunScript-E2EDemo.sh
@@ -0,0 +1,11 @@
+#!/bin/bash -e 
+
+# This script is for use in the API Training Course
+# Written by John Fitzpatrick, March 2013
+
+CURRDBINSTANCE="969GBPOIMG8QI" #Current DB Instance ID
+CLOUD="1"                      #Target Cloud ID number
+
+curl -i -H X_API_VERSION:1.5 -b ~/mycookie -X POST \
+-d recipe_name="db::do_dump_import" \
+https://us-3.rightscale.com/api/clouds/$CLOUD/instances/$CURRDBINSTANCE/run_executable
