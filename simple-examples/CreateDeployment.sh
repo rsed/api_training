fd5700313299ab5881a33a96c7f1031283748e0e
johnfitzpatrick <john.fitzpatrick@rightscale.com>
2013-03-14 12:43:44 +0000

    minor update

---
+++ simple-examples/CreateDeployment.sh
@@ -0,0 +1,7 @@
+#!/bin/bash -e
+
+MYNAME="myname"
+
+curl -i -H X_API_VERSION:1.5 -b ~/mycookie -X POST \
+-d deployment[name]="$MYNAME Deployment for API Sandbox" \
+https://us-3.rightscale.com/api/deployments.xml
