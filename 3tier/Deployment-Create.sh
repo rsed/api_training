34429419f8b54f020de3221107eea49ef2bf78f1
johnfitzpatrick <john.fitzpatrick@rightscale.com>
2013-03-15 11:34:05 +0000

    added 'if' statement to Deployment Create Script

--- 3tier/Deployment-Create.sh
+++ 3tier/Deployment-Create.sh
@@ -1,11 +1,18 @@
 #!/bin/bash -e
 
-# This script is for use in the API Training Course
-# Written by John Fitzpatrick, March 2013
+#!/bin/bash -e
 
 MYNAME="myname"       #Replace with your name
 
+read -p \
+"Are you sure you want to create the Deployment '$MYNAME 3 Tier Deployment'? (y/n): "  -r
+if [[ $REPLY =~ ^[Yy]$ ]]
+then
+ 
 curl -i -H X_API_VERSION:1.5 -b ~/mycookie -X POST \
 -d deployment[name]="$MYNAME 3 Tier Deployment" \
 -d deployment[description]="3 Tier Deployment created by API" \
 https://us-3.rightscale.com/api/deployments.xml
+
+fi
+
