e6325c4d105b4cbfba10acb1f6938f076c3e1659
johnfitzpatrick <john.fitzpatrick@rightscale.com>
2013-03-19 18:43:29 +0000

    Update ServerTemplates_Import.sh

--- 3tier/ServerTemplates_Import.sh
+++ 3tier/ServerTemplates_Import.sh
@@ -10,15 +10,15 @@ DB_ID="47475"
 echo " Load Balancer ServerTemplate"
 curl -i -H X_API_VERSION:1.5 -b ~/mycookie -X POST \
 https://us-3.rightscale.com/api/publications/$LB_ID/import \
-| tee output\ServerTemplates_Import.out
+| tee output/ServerTemplates_Import.out
 
 echo " Application Server ServerTemplate"
 curl -i -H X_API_VERSION:1.5 -b ~/mycookie -X POST \
 https://us-3.rightscale.com/api/publications/$APP_ID/import \
-| tee -a output\ServerTemplates_Import.out
+| tee -a output/ServerTemplates_Import.out
 
 echo " Database ServerTemplate"
 curl -i -H X_API_VERSION:1.5 -b ~/mycookie -X POST \
 https://us-3.rightscale.com/api/publications/$DB_ID/import \
-| tee -a output\ServerTemplates_Import.out
+| tee -a output/ServerTemplates_Import.out
 
