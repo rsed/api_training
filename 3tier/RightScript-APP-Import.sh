5f0d2a4e4c8cb32035b64adaae5bb455dd902780
johnfitzpatrick <john.fitzpatrick@rightscale.com>
2013-03-19 18:33:51 +0000

    Update RightScript-APP-Import.sh

--- 3tier/RightScript-APP-Import.sh
+++ 3tier/RightScript-APP-Import.sh
@@ -7,4 +7,5 @@ RIGHTSCRIPTID="48616"
 
 echo "***Importing RightScript '3Tier-End-to-End Demo [RSED]'***"
 curl -i -H X_API_VERSION:1.5 -b ~/mycookie -X POST \
-https://us-3.rightscale.com/api/publications/$RIGHTSCRIPTID/import
+https://us-3.rightscale.com/api/publications/$RIGHTSCRIPTID/import \
+| tee output/RightScript-APP-Import.out
