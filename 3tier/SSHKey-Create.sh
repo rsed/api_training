5b925f8abd81052ebbfbc9f4d1c387d08054cf80
johnfitzpatrick <john.fitzpatrick@rightscale.com>
2013-03-19 18:34:35 +0000

    Update SSHKey-Create.sh

--- 3tier/SSHKey-Create.sh
+++ 3tier/SSHKey-Create.sh
@@ -8,4 +8,5 @@ CLOUD="1"        #Target Cloud ID number
 
 curl -i -H X_API_VERSION:1.5 -b ~/mycookie -X POST \
 -d ssh_key[name]="$MYNAME SSH Key" \
-https://us-3.rightscale.com/api/clouds/$CLOUD/ssh_keys.xml
+https://us-3.rightscale.com/api/clouds/$CLOUD/ssh_keys.xml \
+| tee output/SSHKey-Create.out
