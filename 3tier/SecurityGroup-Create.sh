edebd0fc9c11eb06aa4aadeca614bf6dba4dcb9b
johnfitzpatrick <john.fitzpatrick@rightscale.com>
2013-03-19 18:35:09 +0000

    Update SecurityGroup-Create.sh

--- 3tier/SecurityGroup-Create.sh
+++ 3tier/SecurityGroup-Create.sh
@@ -9,4 +9,5 @@ CLOUD="1"       #Cloud ID for your chosen cloud
 curl -i -H X_API_VERSION:1.5 -b ~/mycookie -X POST \
 -d security_group[name]="$MYNAME Security Group2" \
 -d security_group[description]="$MYNAME API Training Security Group" \
-https://us-3.rightscale.com/api/clouds/$CLOUD/security_groups
+https://us-3.rightscale.com/api/clouds/$CLOUD/security_groups \
+| tee output/SecurityGroup-Create.out
