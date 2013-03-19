6e5cfde0d15772618f705d312bf306296ceaceab
johnfitzpatrick <john.fitzpatrick@rightscale.com>
2013-03-19 18:44:15 +0000

    Update ServerLB-Launch.sh

--- 3tier/ServerLB-Launch.sh
+++ 3tier/ServerLB-Launch.sh
@@ -7,8 +7,10 @@ LBSERVER1="670392003"  #Server ID for 'myname Load Balancer 1'
 LBSERVER2="670395003"  #Server ID for 'myname Load Balancer 2'
 
 curl -i -H X_API_VERSION:1.5 -b ~/mycookie -X POST \
-https://us-3.rightscale.com/api/servers/$LBSERVER1/launch.xml
+https://us-3.rightscale.com/api/servers/$LBSERVER1/launch.xml \
+| tee output/ServerLB-Launch.out
 
 curl -i -H X_API_VERSION:1.5 -b ~/mycookie -X POST \
-https://us-3.rightscale.com/api/servers/$LBSERVER2/launch.xml
+https://us-3.rightscale.com/api/servers/$LBSERVER2/launch.xml \
+| tee -a output/ServerLB-Launch.out
 
