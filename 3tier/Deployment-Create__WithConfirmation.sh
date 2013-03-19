462cc7a256bee2eb676d42be0ebab43576181f47
johnfitzpatrick <john.fitzpatrick@rightscale.com>
2013-03-19 18:32:09 +0000

    Update Deployment-Create__WithConfirmation.sh

--- 3tier/Deployment-Create__WithConfirmation.sh
+++ 3tier/Deployment-Create__WithConfirmation.sh
@@ -12,7 +12,8 @@ then
 curl -i -H X_API_VERSION:1.5 -b ~/mycookie -X POST \
 -d deployment[name]="$MYNAME 3 Tier Deployment" \
 -d deployment[description]="3 Tier Deployment created by API" \
-https://us-3.rightscale.com/api/deployments.xml
+https://us-3.rightscale.com/api/deployments.xml \
+| tee output/Deployment-Create__WithConfirmation.out
 
 fi
 
