1937b012a83be4605e755ea50a923ed385eb9c2c
johnfitzpatrick <john.fitzpatrick@rightscale.com>
2013-03-24 17:37:31 +0000

    moved automated scripts

--- 3tier/automated/Servers-Create--Automatic.sh
+++ 3tier/automated/Servers-Create--Automatic.sh
@@ -7,22 +7,22 @@ MYNAME="myname"         # Replace with your name
 CLOUD="1"               # Specify Cloud to add Server to
 
 #DEPLOYMENT="350944003"  # Deployment to add Server to
-DEPLOYMENT=`grep Location output/Deployment-Create.out |cut -c 28-|tr -d '\r'`
+DEPLOYMENT=`grep Location ../output/Deployment-Create.out |cut -c 28-|tr -d '\r'`
 
 #LB_ST="282920003"       # Set the Load Balancer Server ServerTemplate
-LB_ST=`grep Location output/ServerTemplates_Import.out|cut -c 33-|tr -d '\r'|sed -n 1p`
+LB_ST=`grep Location ../output/ServerTemplates_Import.out|cut -c 33-|tr -d '\r'|sed -n 1p`
 
 #APP_ST="282922003"      # Set the APP Server ServerTemplate
-APP_ST=`grep Location output/ServerTemplates_Import.out|cut -c 33-|tr -d '\r'|sed -n 2p`
+APP_ST=`grep Location ../output/ServerTemplates_Import.out|cut -c 33-|tr -d '\r'|sed -n 2p`
 
 #DB_ST="282921003"       # Set the Database Server ServerTemplate
-DB_ST=`grep Location output/ServerTemplates_Import.out|cut -c 33-|tr -d '\r'|sed -n 3p`
+DB_ST=`grep Location ../output/ServerTemplates_Import.out|cut -c 33-|tr -d '\r'|sed -n 3p`
 
 #SG="50K6AE2MB3LDL"      # Set the Security Group
-SG=`grep Location output/SecurityGroup-Create.out |cut -c 41-|tr -d '\r'`
+SG=`grep Location ../output/SecurityGroup-Create.out |cut -c 41-|tr -d '\r'`
 
 #SSH="2OSIPDJU7Q55G"     # Set the SSH Key
-SSH=`grep Location output/SSHKey-Create.out |cut -c 34-|tr -d '\r'`
+SSH=`grep Location ../output/SSHKey-Create.out |cut -c 34-|tr -d '\r'`
 
 
 
@@ -36,7 +36,7 @@ curl -i -H X_API_VERSION:1.5 -b ~/mycookie -X POST \
 -d server[instance][security_group_hrefs][]=/api/clouds/$CLOUD/security_groups/$SG \
 -d server[instance][ssh_key_href]=/api/clouds/$CLOUD/ssh_keys/$SSH \
 https://us-3.rightscale.com/api/servers \
-| tee output/Servers-Create.out
+| tee ../output/Servers-Create.out
 
 echo " Application Server 1"
 curl -i -H X_API_VERSION:1.5 -b ~/mycookie -X POST \
@@ -48,7 +48,7 @@ curl -i -H X_API_VERSION:1.5 -b ~/mycookie -X POST \
 -d server[instance][security_group_hrefs][]=/api/clouds/$CLOUD/security_groups/$SG \
 -d server[instance][ssh_key_href]=/api/clouds/$CLOUD/ssh_keys/$SSH \
 https://us-3.rightscale.com/api/servers \
-| tee -a output/Servers-Create.out
+| tee -a ../output/Servers-Create.out
 
 echo " Database Server"
 curl -i -H X_API_VERSION:1.5 -b ~/mycookie -X POST \
@@ -60,5 +60,5 @@ curl -i -H X_API_VERSION:1.5 -b ~/mycookie -X POST \
 -d server[instance][security_group_hrefs][]=/api/clouds/$CLOUD/security_groups/$SG \
 -d server[instance][ssh_key_href]=/api/clouds/$CLOUD/ssh_keys/$SSH \
 https://us-3.rightscale.com/api/servers \
-| tee -a output/Servers-Create.out
+| tee -a ../output/Servers-Create.out
 
