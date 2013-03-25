9eac83e77750e8dcc9de22939b37c2f6f5fa22d8
johnfitzpatrick <john.fitzpatrick@rightscale.com>
2013-03-25 10:30:43 +0000

    fixed typos

--- 3tier/Deployment-Create.sh
+++ 3tier/Deployment-Create.sh
@@ -1,7 +1,7 @@
 #!/bin/bash -e
 
 #MYNAME="myname"                                        # Replace with your name
-MYNAME="grep MYNAME LabInfo | cut -c 8-|tr -d '\r'"     # Or, get your name form LabInfo file
+MYNAME=`grep MYNAME LabInfo | cut -c 8-|tr -d '\r'`     # Or, get your name form LabInfo file
 
 curl -i -H X_API_VERSION:1.5 -b ~/mycookie -X POST \
 -d deployment[name]="$MYNAME 3 Tier Deployment" \
--- 3tier/SecurityGroup-Create.sh
+++ 3tier/SecurityGroup-Create.sh
@@ -4,7 +4,7 @@
 # Written by John Fitzpatrick, March 2013
 
 #MYNAME="myname"                                        # Replace with your name
-MYNAME="grep MYNAME LabInfo | cut -c 8-|tr -d '\r'"     # Or, get your name form LabInfo file
+MYNAME=`grep MYNAME LabInfo | cut -c 8-|tr -d '\r'`     # Or, get your name form LabInfo file
 #CLOUD="1"                                              # Specify the Cloud to add the Server Array to
 CLOUD=`grep CLOUD LabInfo | cut -c 7-|tr -d '\r'`       # Or, get cloud id form LabInfo file
 
