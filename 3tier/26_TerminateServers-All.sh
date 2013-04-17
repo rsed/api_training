d726fd8e69a4e3a0e9d268095469e09ab2399965
johnfitzpatrick <john.fitzpatrick@rightscale.com>
2013-04-17 15:44:55 +0000

    updated 26_TerminateServers-All.sh

--- 3tier/26_TerminateServers-All.sh
+++ 3tier/26_TerminateServers-All.sh
@@ -5,11 +5,12 @@
 # Written by John Fitzpatrick, March 2013
 
 CLOUD=`grep CLOUD LabInfo | cut -c 7-|tr -d '\r'`
+DEPLOYMENT=`grep Location output/06_Deployment-Create.sh.out |cut -c 28-|tr -d '\r'`
 
 cd /opt/api/3tier
 
 curl -i -H X-API-Version:1.5 -b ~/mycookie -X POST \
+-d deployment_href=$DEPLOYMENT \
 -d terminate_all=true \
 https://us-3.rightscale.com/api/clouds/$CLOUD/instances/multi_terminate \
 | tee output/${0##*/}.out
-
