80ec0e4c03dc9d72fbcb01585ec082fa8f611096
johnfitzpatrick <john.fitzpatrick@rightscale.com>
2013-04-19 19:13:29 +0000

    updates

--- right-api-client/ruby3tier/3Tier-Create.rb
+++ right-api-client/ruby3tier/3Tier-Create.rb
@@ -1,3 +1,5 @@
+#!/usr/bin/ruby
+#
 require 'rubygems'
 require 'pp'
 require 'right_api_client'
@@ -13,6 +15,17 @@ lb_server_template_href = @client.server_templates.index(:filter => ['name==Load
 app_server_template_href = @client.server_templates.index(:filter => ['name==PHP App Server (v13.2.1) [RSED]']).first.href
 db_server_template_href = @client.server_templates.index(:filter => ['name==Database Manager for MySQL 5.5 (v13.2.1) [RSED]']).first.href
 
+def labinfo
+  parsed_file=YAML.load(File.read(File.join(File.dirname(__FILE__),"labinfo.yaml")))
+  @dbfqdn=parsed_file["inputs"]["dbfqdn"]
+  @ddnsid=parsed_file["inputs"]["ddnsid"]
+  @cloud=parsed_file["inputs"]["cloud"]
+  @myname=parsed_file["inputs"]["myname"]
+  @dblineage=parsed_file["inputs"]["dblineage"]
+  @dbschema=parsed_file["inputs"]["dbschema"]
+end
+
+labinfo
 
 puts "What would you like to call your deployment?"
 depname = STDIN.gets.chomp()
@@ -74,30 +87,30 @@ newdb_server.api_methods
 #Set Inputs
 puts "Setting Inputs..."
 get_deployment(depname)
-add_input('appapp/database_name','text:$DBSCHEMA')
-add_input('appapp_php/modules_list','array:php53u-mysql,php53u-pecl-memcache')
-add_input('applb/session_stickiness','text:false')
-add_input('appweb_apache/mpm','text:prefork')
-add_input('appdb/admin/user','text:adminuser')
-add_input('appdb/admin/password','text:adminpassword')
-add_input('appdb/application/user','text:appuser123')
-add_input('appdb/application/password','text:apppasword123')
-add_input('appdb/backup/lineage','text:$DBLINAGE')
-add_input('appdb/dns/master/fqdn','text:$DBFQDN')
-add_input('appdb/dns/master/id','text:$DDNSID')
-add_input('appdb/dump/container','cred:DUMP_CONTAINER')
-add_input('appdb/dump/database_name','text:$DBSCHEMA')
-add_input('appdb/dump/prefix','text:sampledb')
-add_input('appdb/dump/storage_account_id','cred:AWS_ACCESS_KEY_ID')
-add_input('appdb/dump/storage_account_secret','cred:AWS_SECRET_ACCESS_KEY')
-add_input('appdb/replication/user','text:repluser')
-add_input('appdb/replication/password','text:replpassword')
-add_input('appdb/provider_type','text:db_mysql_5.5')
-add_input('apprepo/default/repository','text:git://github.com/rightscale/examples.git')
-add_input('apprepo/default/revision','text:unified_php')
-add_input('appsys_dns/choice','text:DNSMadeEasy')
-add_input('appsys_dns/password','cred:DNS_PASSWORD')
-add_input('appsys_dns/user','cred:DNS_USER')
+add_inputs('app/database_name','text:#[dbschema]') #Cannot be missing (RuntimeError)
+add_inputs('app_php/modules_list','array:php53u-mysql,php53u-pecl-memcache') #Unsupported input value
+add_inputs('lb/session_stickiness','text:false')
+add_inputs('web_apache/mpm','text:prefork')
+add_inputs('db/admin/user','text:adminuser')
+add_inputs('db/admin/password','text:adminpassword')
+add_inputs('db/application/user','text:appuser123') #The selected credential does not exist on this account
+add_inputs('db/application/password','text:apppasword123') #The selected credential does not exist on this account
+add_inputs('db/backup/lineage','text:$DBLINAGE')
+add_inputs('db/dns/master/fqdn','text:#[dbfqdn]') #Cannot be missing
+add_inputs('db/dns/master/id','text:#[ddnsid]')
+add_inputs('db/dump/container','cred:DUMP_CONTAINER')
+add_inputs('db/dump/database_name','text:#[dbschema]')
+add_inputs('db/dump/prefix','text:sampledb')
+add_inputs('db/dump/storage_account_id','cred:AWS_ACCESS_KEY_ID')
+add_inputs('db/dump/storage_account_secret','cred:AWS_SECRET_ACCESS_KEY')
+add_inputs('db/replication/user','text:repluser')
+add_inputs('db/replication/password','text:replpassword')
+add_inputs('db/provider_type','text:db_mysql_5.5') #Cannot be missing
+add_inputs('repo/default/repository','text:git://github.com/rightscale/examples.git') #Cannot be missing!!
+add_inputs('repo/default/revision','text:unified_php')
+add_inputs('sys_dns/choice','text:DNSMadeEasy')
+add_inputs('sys_dns/password','cred:DNS_PASSWORD')
+add_inputs('sys_dns/user','cred:DNS_USER')
 deployment_set_inputs(@deployment, @inputs)
 
 #Launch the Servers
