7c5841ade8c7243ba8d045f98c656d2a9a0833c7
johnfitzpatrick <john.fitzpatrick@rightscale.com>
2013-04-19 19:44:49 +0000

    updates

--- right-api-client/ruby3tier/3Tier-Create.rb
+++ right-api-client/ruby3tier/3Tier-Create.rb
@@ -93,8 +93,10 @@ add_inputs('lb/session_stickiness','text:false')
 add_inputs('web_apache/mpm','text:prefork')
 add_inputs('db/admin/user','text:adminuser')
 add_inputs('db/admin/password','text:adminpassword')
-add_inputs('db/application/user','text:appuser123') #The selected credential does not exist on this account
-add_inputs('db/application/password','text:apppasword123') #The selected credential does not exist on this account
+#add_inputs('db/application/user','text:appuser123') #The selected credential does not exist on this account
+add_inputs('db/application/user','text:appuser') #The selected credential does not exist on this account
+#add_inputs('db/application/password','text:apppasword123') #The selected credential does not exist on this account
+add_inputs('db/application/password','text:apppasword') #The selected credential does not exist on this account
 add_inputs('db/backup/lineage','text:$DBLINAGE')
 add_inputs('db/dns/master/fqdn','text:#[dbfqdn]') #Cannot be missing
 add_inputs('db/dns/master/id','text:#[ddnsid]')
@@ -106,7 +108,9 @@ add_inputs('db/dump/storage_account_secret','cred:AWS_SECRET_ACCESS_KEY')
 add_inputs('db/replication/user','text:repluser')
 add_inputs('db/replication/password','text:replpassword')
 add_inputs('db/provider_type','text:db_mysql_5.5') #Cannot be missing
+add_inputs('db/provider_type','text:"db_mysql_5.5"') #Cannot be missing
 add_inputs('repo/default/repository','text:git://github.com/rightscale/examples.git') #Cannot be missing!!
+add_inputs('repo/default/repository','text:"git://github.com/rightscale/examples.git"') #Cannot be missing!!
 add_inputs('repo/default/revision','text:unified_php')
 add_inputs('sys_dns/choice','text:DNSMadeEasy')
 add_inputs('sys_dns/password','cred:DNS_PASSWORD')
