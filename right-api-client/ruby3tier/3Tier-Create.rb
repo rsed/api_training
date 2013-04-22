f99cc665a40eb3cd99072d1c1ec7679dc5e07846
johnfitzpatrick <john.fitzpatrick@rightscale.com>
2013-04-22 16:48:18 +0000

    updated 3Tier-Create.rb

--- right-api-client/ruby3tier/3Tier-Create.rb
+++ right-api-client/ruby3tier/3Tier-Create.rb
@@ -97,7 +97,7 @@ puts "Setting Inputs..."
 
 get_deployment(depname)
 
-add_inputs('app/database_name','text:#[@dbschema]') #Cannot be missing (RuntimeError) JF: Had to add array to 'add_inputs'
+add_inputs('app/database_name','text:'"#{@dbschema}") #Cannot be missing (RuntimeError) JF: Had to add array to 'add_inputs'
 add_inputs('app_php/modules_list','array:php53u-mysql,php53u-pecl-memcache') #Unsupported input value
 add_inputs('lb/session_stickiness','text:false')
 add_inputs('web_apache/mpm','text:prefork')
@@ -106,10 +106,10 @@ add_inputs('db/admin/password','text:adminpassword')
 add_inputs('db/application/user','text:appuser123') #The selected credential does not exist on this account
 add_inputs('db/application/password','text:apppasword123') #The selected credential does not exist on this account
 add_inputs('db/backup/lineage','text:$DBLINAGE')
-add_inputs('db/dns/master/fqdn','text:#[dbfqdn]') #Cannot be missing
-add_inputs('db/dns/master/id','text:#[ddnsid]')
+add_inputs('db/dns/master/fqdn','text:'"#{@dbfqdn}") #Cannot be missing
+add_inputs('db/dns/master/id','text:'"#{@ddnsid}")
 add_inputs('db/dump/container','cred:DUMP_CONTAINER')
-add_inputs('db/dump/database_name','text:#[dbschema]')
+add_inputs('db/dump/database_name','text:'"#{@dbschema}")
 add_inputs('db/dump/prefix','text:sampledb')
 add_inputs('db/dump/storage_account_id','cred:AWS_ACCESS_KEY_ID')
 add_inputs('db/dump/storage_account_secret','cred:AWS_SECRET_ACCESS_KEY')
@@ -124,9 +124,9 @@ add_inputs('sys_dns/choice','text:DNSMadeEasy')
 add_inputs('sys_dns/password','cred:DNS_PASSWORD')
 add_inputs('sys_dns/user','cred:DNS_USER')
 
-
-deployment_set_inputs(@deployment, @inputs)
+#deployment_set_inputs(@deployment, @inputs)
 #deployment_set_inputs(depname, @inputs)
+@client.deployments(:deployment => {:name => depname}).show.inputs.multi_update(list_inputs(@inputs))
 
 #Launch the Servers
 puts "Starting Servers...."
